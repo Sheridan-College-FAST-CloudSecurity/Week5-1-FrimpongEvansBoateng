e9405cc65c71f137019c3a64016cf9e20fe1f288
Evans Frimpong <frimpoev@sheridancollege.ca>
2025-02-04 00:38:41 +0000

    Reverting the error simulation

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def hello():
-       return "Hello, DevSecOps - modified!"
+       return "Hello, DevSecOps!"
+
+if __name__ == "__main__":
+   app.run()
